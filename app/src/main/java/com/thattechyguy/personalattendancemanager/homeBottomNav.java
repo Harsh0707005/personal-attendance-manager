3988470ddf544f588309d59cbd32ff101d991a7c
Harsh0707005 <harshmaster07705@gmail.com>
2024-02-28 23:27:58 +0530

    Commit

--- app/src/main/java/com/thattechyguy/personalattendancemanager/homeBottomNav.java
+++ app/src/main/java/com/thattechyguy/personalattendancemanager/homeBottomNav.java
@@ -1,14 +1,19 @@
 package com.thattechyguy.personalattendancemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class homeBottomNav extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +21,36 @@ public class homeBottomNav extends AppCompatActivity {
         setContentView(R.layout.activity_home_bottom_nav);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.fragment);
 
         bottomNavigationView.setItemIconTintList(null);
 
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.dashboard_menu){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new DashboardFragment())
+                            .commit();
+                    return true;
+                }else if (item.getItemId()==R.id.schedule_manage_menu){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new ScheduleManageFragment())
+                            .commit();
+                    return true;
+                }else if (item.getItemId()==R.id.settings_menu){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new SettingsFragment())
+                            .commit();
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
