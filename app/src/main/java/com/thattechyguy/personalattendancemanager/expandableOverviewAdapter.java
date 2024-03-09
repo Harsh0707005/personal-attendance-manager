c46d23147ce777de6e06ba9c5ac95afe2b94da58
Harsh0707005 <harshmaster07705@gmail.com>
2024-03-10 00:34:30 +0530

    Commit

--- app/src/main/java/com/thattechyguy/personalattendancemanager/expandableOverviewAdapter.java
+++ app/src/main/java/com/thattechyguy/personalattendancemanager/expandableOverviewAdapter.java
@@ -1,12 +1,18 @@
 package com.thattechyguy.personalattendancemanager;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -93,11 +99,79 @@ public class expandableOverviewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.class_overview, null);
+        }
+
+        PieChart classPie = convertView.findViewById(R.id.classPie);
+        TextView numAttendedTextView = convertView.findViewById(R.id.numAttendedTextView);
+        TextView numTotalTextView = convertView.findViewById(R.id.numTotalTextView);
+        TextView numPercentTextView = convertView.findViewById(R.id.numPercentTextView);
+
+        try{
+            Integer numAttended = getChild(groupPosition, childPosition).get("numAttended");
+            Integer numTotal = getChild(groupPosition, childPosition).get("numTotal");
+            String percent = String.valueOf((numAttended*100)/numTotal);
+
+            showPieChart(classPie, getChild(groupPosition, childPosition));
+            numAttendedTextView.setText(numAttended.toString());
+            numTotalTextView.setText(numTotal.toString());
+            numPercentTextView.setText(percent+"%");
+
+        }catch(Exception e){
+            Log.d("harsh", e.getMessage());
+        }
+
+
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return false;
+    }
+
+    private void showPieChart(PieChart pieChart, HashMap<String, Integer> attendance){
+
+        try{
+
+            ArrayList<PieEntry> pieEntries = new ArrayList<>();
+            String label = "";
+
+            //initializing colors for the entries
+            ArrayList<Integer> colors = new ArrayList<>();
+            colors.add(Color.parseColor("#304567"));
+            colors.add(Color.parseColor("#309967"));
+            colors.add(Color.parseColor("#476567"));
+            colors.add(Color.parseColor("#890567"));
+            colors.add(Color.parseColor("#a35567"));
+            colors.add(Color.parseColor("#ff5f67"));
+            colors.add(Color.parseColor("#3ca567"));
+
+            //input data and fit data into pie chart entry
+            for(String type: attendance.keySet()){
+                if (type.equals("numAttended")) pieEntries.add(new PieEntry(attendance.get(type).floatValue(), "Attended"));
+                if (type.equals("numTotal")) pieEntries.add(new PieEntry(attendance.get(type).floatValue(), "Total"));
+            }
+
+            //collecting the entries with label name
+            PieDataSet pieDataSet = new PieDataSet(pieEntries,"");
+            //setting text size of the value
+            pieDataSet.setValueTextSize(10f);
+            pieDataSet.setValueTextColor(Color.WHITE);
+            //providing color list for coloring different entries
+            pieDataSet.setColors(colors);
+            //grouping the data set from entry to chart
+            PieData pieData = new PieData(pieDataSet);
+            //showing the value of the entries, default true if not set
+            pieData.setDrawValues(true);
+
+            pieChart.setData(pieData);
+            pieChart.invalidate();
+        }catch(Exception e){
+            Log.d("harsh", e.getMessage());
+        }
     }
 }
